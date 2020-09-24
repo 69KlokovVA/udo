03a367b7359ee75c88a876207d882f5a9a037b05
Vasiliy Klokov <vasiliyklokov@gmail.com>
2020-09-24 19:07:35 +0700

    деление чисел

--- src/main/java/Greeting.java
+++ src/main/java/Greeting.java
@@ -1,5 +1,8 @@
 public class Greeting {
     public static void main(String... args) {
         System.out.println("Здравствуйте, Иван Валентинович!");
+        int a=355;
+        int b=113;
+        System.out.println((float)a/b);
     }
 }
