ed2826952f9e78bb3d45dda6aaeac0a8defd6e24
Vasiliy Klokov <vasiliyklokov@gmail.com>
2020-09-24 22:55:26 +0700

    получение PI делением целых чисел

---
+++ src/main/java/CalcAB.java
@@ -0,0 +1,5 @@
+public class CalcAB {
+    public static double pi(int a, int b) {
+        return (double)a/b;
+    }
+}
--- src/main/java/Greeting.java
+++ src/main/java/Greeting.java
@@ -1,14 +1,15 @@
 public class Greeting {
     public static void main(String... args) {
         System.out.println("Здравствуйте, Иван Валентинович!");
-        int a=355;
-        int b=113;
-        double p = pi(a,b);
-        System.out.println(p);
-        System.out.println(Math.PI);
-    }
-
-    public static double pi(int a, int b) {
-        return (double)a/b;
+        int iab = 400;
+        double delta = 0.00001;
+        for (int i = 1; i < iab; i++) {
+            for (int j = iab; j > 2; j--) {
+                double p = CalcAB.pi(i, j);
+                if (Math.abs((p - Math.PI)/p)  < delta) {
+                    System.out.println(i + " " + j + " " + p);
+                }
+            }
+        }
     }
 }
